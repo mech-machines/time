b2e4b2fd6fb42f1b732df397b46fe258cf6747e5
cmontella <cmontella@live.com>
2020-05-10 15:08:58 -0400

    use channel and macros in utils

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-#[macro_use]
 extern crate mech_core;
+#[macro_use]
 extern crate mech_utilities;
+extern crate crossbeam_channel;
 //use mech_core::{Interner, Transaction};
 //use mech_core::Value;
 
