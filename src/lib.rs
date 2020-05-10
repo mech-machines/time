1b43f34c5ecc306407e1b94642843234c1528beb
cmontella <cmontella@live.com>
2020-05-10 00:27:59 -0400

    example basic machine implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+#[macro_use]
 extern crate mech_core;
-extern crate mech_program;
-use mech_core::{Interner, Transaction};
-use mech_core::Value;
+extern crate mech_utilities;
+//use mech_core::{Interner, Transaction};
+//use mech_core::Value;
 
 pub mod timer;
--- src/timer.rs
+++ src/timer.rs
@@ -1,31 +1,64 @@
 extern crate time;
 use std::time::Duration;
-use mech_program::{RunLoopMessage, Watcher};
-use mech_core::{Interner, Transaction, Change};
-use mech_core::Hasher;
-use mech_core::Value;
+use mech_core::{Hasher, Machine, MachineRegistrar};
 use std::sync::mpsc::{self, Sender};
 use std::thread::{self};
-use std::collections::{HashMap};
-use std::collections::hash_map::{Entry};
 
+
+
+export_machine!(time_timer, time_timer_reg);
+
+extern "C" fn time_timer_reg(registrar: &mut dyn MachineRegistrar) {
+  registrar.register_machine(Box::new(Timer));
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Timer;
+
+impl Machine for Timer {
+
+    fn name(&self) -> String {
+      "time/timer".to_string()
+    }
+
+    fn id(&self) -> u64 {
+      Hasher::hash_string(self.name())
+    }
+
+    fn call(&self) -> Result<(), String> {
+        thread::spawn(move || {
+          let duration = Duration::from_millis(1000);
+          let mut counter = 0;
+          loop {
+            println!("{}", counter);
+            thread::sleep(duration);
+            counter = counter + 1; 
+          }
+        });
+        Ok(())
+    }
+}
+
+
+/*
 pub struct Timer {
   name: String,
-  columns: usize,
-  outgoing: Sender<RunLoopMessage>,
-  timers: HashMap<u64, (usize, Sender<()>)>
+  //columns: usize,
+  //outgoing: Sender<RunLoopMessage>,
+  //timers: HashMap<u64, (usize, Sender<()>)>
 }
 
 impl Timer {
-  pub fn new(outgoing: Sender<RunLoopMessage>) -> Timer {
-    Timer { name: "time/timer".to_string(), outgoing, timers: HashMap::new(), columns: 10 }
+  pub fn new() -> Timer {
+    Timer { name: "time/timer".to_string()}//, outgoing, timers: HashMap::new(), columns: 10 }
   }
 }
 
-impl Watcher for Timer {
+impl Machine for Timer {
   fn get_name(& self) -> String {
     self.name.clone()
   }
+  /*
   fn set_name(&mut self, name: &str) {
     self.name = name.to_string();
   }
@@ -39,6 +72,7 @@ impl Watcher for Timer {
     for change in diff.names {
       match change {
         Change::RenameColumn{table, column_ix, column_alias} => {
+          /*
           let resolution_column = Hasher::hash_str("period");
           if column_alias == resolution_column {
             let outgoing = self.outgoing.clone();
@@ -70,10 +104,11 @@ impl Watcher for Timer {
                 }
               }
             });
-          }
+          }*/
         },
         _ => (),
       }
     }  
-  }
+  }*/
 }
+*/
