09de52ce8221413ede9c19989b621873efe891d3
cmontella <cmontella@live.com>
2023-01-04 19:21:47 -0500

    remove unused dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
 extern crate mech_core;
-#[macro_use]
 extern crate mech_utilities;
 extern crate crossbeam_channel;
 #[macro_use]
 extern crate lazy_static;
-use mech_core::*;
-use mech_utilities::*;
-use std::collections::HashMap;
 
 pub mod timer;
